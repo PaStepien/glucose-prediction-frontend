139e2d25940508f6bce0035a64c31b320ed6eed7
PaStepien <pstepien@interia.pl>
2026-04-16 11:00:21 +0200

    adde the simple structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,7 +26,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+       <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
