5c860fcc3b27cad135abe91a0bc69210e8689a68
Maria-Lavinia Otelea <mlo@frankly.dk>
2026-04-28 22:13:30 +0200

    clwanup

--- components/auth/LogoutButton.tsx
+++ components/auth/LogoutButton.tsx
@@ -1,14 +1,11 @@
 import { supabase } from '@/auth/lib/supabase';
-import { useRouter } from 'expo-router';
 import { Alert, Text, TouchableOpacity } from 'react-native';
 
 export const LogoutButton = () =>{
-  const router = useRouter();
 
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) Alert.alert('Logout error', error.message);
-    router.replace('/');
   };
 
   return (
